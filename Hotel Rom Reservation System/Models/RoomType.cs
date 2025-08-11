957809d9cc7a034f2b8406af8d3eee3428ee05e3
rozaliya02 <103183688+rozaliya02@users.noreply.github.com>
2025-08-11 18:37:15 +0300

    add project skeleton

---
+++ Hotel Rom Reservation System/Models/RoomType.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Hotel_Rom_Reservation_System.Models
+{
+    public class RoomType
+    {
+        public string Name { get; set; }
+        public string Amenities { get; set; }
+        public int MaxOccupancy { get; set; }
+
+    }
+}
