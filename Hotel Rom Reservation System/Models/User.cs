091db69eaeb7f068431352f87dec45b73597a70d
rozaliya02 <103183688+rozaliya02@users.noreply.github.com>
2025-08-11 18:43:17 +0300

    add User and Booking classes

--- Hotel Rom Reservation System/Models/User.cs
+++ Hotel Rom Reservation System/Models/User.cs
@@ -1,12 +1,13 @@
-﻿using System;
+﻿
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Hotel_Rom_Reservation_System.Models
 {
     internal class User
     {
+        public string UsernaName { get; set; }
+        public string Password { get; set; }
+
+        public List<int> BookingHistory { get; set; };
     }
 }
